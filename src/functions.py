2d8eb386e3e15acdcdce8c1b8f8ca795faab0764
marcelochincha <103434714+marcelochincha@users.noreply.github.com>
2022-05-21 09:33:55 -0500

    Count SYMBOLS

--- src/functions.py
+++ src/functions.py
@@ -65,4 +65,20 @@ def rotar180Anti():
 
 def frecCaracteres():
     ascii = escogerASCII()
-    pass
+    
+    charsInd =[]
+    counterInd = []
+
+    print("%-10s | %10s" % ("Caracter","Frecuencia"))
+    print("¯" * 23)
+    for row in ascii:
+        for x in row:
+            if not (x in charsInd):
+                charsInd.append(x)
+                counterInd.append(0)
+                counterInd[charsInd.index(x)] += 1
+            else:
+                counterInd[charsInd.index(x)] += 1
+    
+    for i in range(len(charsInd)):
+        print("%-10s : %-s" % (charsInd[i],counterInd[i]))
