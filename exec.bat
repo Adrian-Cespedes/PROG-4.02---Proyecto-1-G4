0fd7d5363dd7c4c007355f165a57b9c974f3b90e
Adrian Cespedes <adrian.cespedes@utec.edu.pe>
2022-05-23 07:44:56 -0500

    Merge branch 'main' into dev

---
+++ exec.bat
@@ -0,0 +1,5 @@
+@echo off
+
+python src\__main__.py
+
+rd /S /Q src\__pycache__
--- src/functions.py
+++ src/functions.py
@@ -57,12 +57,34 @@ def rotar90Anti():
 
 def rotar180():
     ascii = escogerASCII()
-    pass
+    for f in range(len(ascii)-1,-1,-1):
+        for c in range(len(ascii[f])):
+            print(ascii[f][c],end="")
+        print()
 
 def rotar180Anti():
     ascii = escogerASCII()
-    pass
+    for f in range(len(ascii)-1,-1,-1):
+        for c in range(len(ascii[f])-1,-1,-1):
+            print(ascii[f][c],end="")
+        print()
 
 def frecCaracteres():
     ascii = escogerASCII()
-    pass
+    
+    charsInd =[]
+    counterInd = []
+
+    print("%-10s | %10s" % ("Caracter","Frecuencia"))
+    print("¯" * 23)
+    for row in ascii:
+        for x in row:
+            if not (x in charsInd):
+                charsInd.append(x)
+                counterInd.append(0)
+                counterInd[charsInd.index(x)] += 1
+            else:
+                counterInd[charsInd.index(x)] += 1
+    
+    for i in range(len(charsInd)):
+        print("%-10s : %-s" % (charsInd[i],counterInd[i]))
